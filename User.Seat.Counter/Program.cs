eb8f8b1a9321c1634dd68c8d71a95cff5d2b9301
Jestzer <james@buonopane.email>
2024-10-22 10:00:12 -0400

    fix: Made calls no longer ambiguous.

--- User.Seat.Counter/Program.cs
+++ User.Seat.Counter/Program.cs
@@ -23,7 +23,7 @@ catch (Exception ex)
     return;
 }
 
-string[] lines = fileContents.Split(['\r', '\n', '\t'], StringSplitOptions.RemoveEmptyEntries);
+string[] lines = fileContents.Split(new[] { '\r', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
 int lineNumber = 0;
 
@@ -41,7 +41,8 @@ foreach (string line in lines)
     {
         try
         {
-            string[] lineParts = correctedLine.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+            // Do NOT use a collection expression. Doing so will make the call ambiguous.
+            string[] lineParts = correctedLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             string productName = lineParts[3];
             string[] userAndHostnameParts = lineParts[4].Split(new[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
             string user = userAndHostnameParts[0];
